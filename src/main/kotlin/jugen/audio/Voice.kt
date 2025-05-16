9acf42be06ee34c338e68bfdd97fbf4cc00ef835
Andreas Volkmann <anvolkma@microsoft.com>
2025-05-15 21:14:52 -0700

    Clean

--- src/main/kotlin/jugen/audio/Voice.kt
+++ src/main/kotlin/jugen/audio/Voice.kt
@@ -5,11 +5,4 @@ enum class Voice(val id: String) {
     Xiaoxiao("zh-CN-XiaoxiaoNeural"),
     Yunxi("zh-CN-YunxiNeural"),
     Yunjian("zh-CN-YunjianNeural"),
-//    ZH_CN_YUNJIAN_NEURAL_D("zh-CN-YunjianNeural-D"),
-//    ZH_CN_YUNJIAN_NEURAL_F("zh-CN-YunjianNeural-F"),
-//    ZH_CN_YUNJIAN_NEURAL_M("zh-CN-YunjianNeural-M"),
-//    ZH_CN_YUNJIAN_NEURAL_N("zh-CN-YunjianNeural-N"),
-//    ZH_CN_YUNJIAN_NEURAL_S("zh-CN-YunjianNeural-S"),
-//    ZH_CN_YUNJIAN_NEURAL_T("zh-CN-YunjianNeural-T");
-
 }
